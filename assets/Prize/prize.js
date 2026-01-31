0e5aa7e78af14b3ae3d13d828a09f757ca2b2566
EYC <eycfde@gmail.com>
2026-01-31 10:14:04 -0800

    Update prize.js

--- assets/Prize/prize.js
+++ assets/Prize/prize.js
@@ -1,38 +1,36 @@
 $(document).ready(function() {
-  $(".daz-gift-box").on("click", function (event) {
-    let $this = $(this);
-    
-    if (!$this.hasClass("clicked")) {
-      $this.addClass("clicked");
-      // نمرر العنصر الذي تم الضغط عليه للدالة
-      explodeConfetti($this[0]); 
+    $(".daz-gift-box").on("click", function (event) {
+        if (!$(this).hasClass("clicked")) {
+            $(this).addClass("clicked");
+            // نمرر الـ element الحالي مباشرة للدالة
+            explodeConfetti(this);
+        }
+    });
+
+    function explodeConfetti(boxElement) {
+        const confettiTarget = boxElement.querySelector(".confetti-target");
+        const confettiCount = 75;
+
+        for (let i = 0; i < confettiCount; i++) {
+            const confetti = document.createElement("div");
+            confetti.className = "confetti";
+            
+            // تحديد اتجاهات عشوائية
+            confetti.style.setProperty("--x", Math.random() * 2 - 1); 
+            confetti.style.setProperty("--y", Math.random() * 2 - 1); 
+            
+            confetti.style.backgroundColor = getRandomColor();
+            confettiTarget.appendChild(confetti);
+
+            // مسح العنصر بعد انتهاء الأنيمنيشن لتوفير الذاكرة
+            confetti.addEventListener("animationend", () => {
+                confetti.remove();
+            });
+        }
     }
-  });
 
-  function explodeConfetti(targetElement) {
-    const confettiTarget = targetElement.querySelector(".confetti-target");
-    const confettiCount = 75;
-
-    for (let i = 0; i < confettiCount; i++) {
-      const confetti = document.createElement("div");
-      confetti.className = "confetti";
-      
-      // تعيين اتجاهات عشوائية
-      confetti.style.setProperty("--x", Math.random() * 2 - 1);
-      confetti.style.setProperty("--y", Math.random() + 0.5); // لجعلها تطير للأعلى
-      confetti.style.backgroundColor = getRandomColor();
-      
-      confettiTarget.appendChild(confetti);
-
-      // تنظيف الـ DOM بعد انتهاء الحركة
-      confetti.addEventListener("animationend", () => {
-        confetti.remove();
-      });
+    function getRandomColor() {
+        const colors = ['#ff595e', '#ffca3a', '#8ac926', '#1982c4', '#6a4c93'];
+        return colors[Math.floor(Math.random() * colors.length)];
     }
-  }
-
-  function getRandomColor() {
-    const colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff"];
-    return colors[Math.floor(Math.random() * colors.length)];
-  }
 });
